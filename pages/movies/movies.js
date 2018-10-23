bb80281eddae0c2e749fb754fff22d7c6dc9afbf
Nie Xiaojun <nxjniexiao@163.com>
2018-10-23 11:51:29 +0800

    封装豆瓣请求电影数据的API

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -14,111 +14,54 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    const urlBeingRelease = 'https://api.douban.com/v2/movie/nowplaying';
+    const urlWillBeReleased = 'https://api.douban.com/v2/movie/coming';
+    const urlHighScoreMovies = 'https://api.douban.com/v2/movie/top250';
     // 向服务器发起请求：正在热映的电影
-    wx.request({
-      url: 'https://api.douban.com/v2/movie/nowplaying?apikey=0df993c66c0c636e29ecbb5344252a4a',
-      method: 'GET',
-      header: {
-        'Content-Type': 'json'
-      },
-      success: (res) => {
-        if(res.statusCode === 200){
-          if (res.data.entries) {
-            // 注：数组长度不够时，slice()会返回数组的副本
-            this.setData({
-              moviesBeingRelease: res.data.entries.slice(0, 3)
-            });
-          }
-        }
-      }
-    });
+    this.getMoviesList(urlBeingRelease, 'moviesBeingRelease');
     // 向服务器发起请求：即将上映的电影
-    wx.request({
-      url: 'http://api.douban.com/v2/movie/coming?apikey=0df993c66c0c636e29ecbb5344252a4a',
-      method: 'GET',
-      header: {
-        'Content-Type': 'json'
-      },
-      success: (res) => {
-        if (res.statusCode === 200) {
-          if (res.data.entries) {
-            this.setData({
-              moviesWillBeReleased: res.data.entries.slice(0, 3)
-            });
-          }
-        }
-      }
-    });
+    this.getMoviesList(urlWillBeReleased, 'moviesWillBeReleased');
     // 向服务器发起请求：高分电影
+    this.getMoviesList(urlHighScoreMovies, 'highScoreMovies');
+  },
+  // 封装获取数据的方法
+  getMoviesList: function(url, moviesListName) {
+    const opt = '?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=3';// 请求前三条
     wx.request({
-      url: 'http://api.douban.com/v2/movie/top250?apikey=0df993c66c0c636e29ecbb5344252a4a',
+      url: url + opt,
       method: 'GET',
       header: {
         'Content-Type': 'json'
       },
       success: (res) => {
         if (res.statusCode === 200) {
-          if (res.data.subjects){
-            // 与其他 API 返回的 json 结构不一样（评分 rating 不一样）
-            const originalMoviesList = res.data.subjects;
-            originalMoviesList.forEach((list, index) => {
-              originalMoviesList[index].rating = originalMoviesList[index].rating.average;
+          // top250 API 返回的 json 结构与其他两个不一样
+          const originalMoviesList = res.data.subjects || res.data.entries;
+          if (originalMoviesList) {
+            // top250 API 中的 rating 为对象，其他的为评分字符串
+            if (moviesListName == 'highScoreMovies') {
+              originalMoviesList.forEach((list, index) => {
+                originalMoviesList[index].rating = originalMoviesList[index].rating.average;
+              })
+            }
+            // 防止API变动，导致返回的电影数目超过3个
+            originalMoviesList.splice(3);
+            // 筛选需要的数据
+            const newMoviesList=[];
+            originalMoviesList.forEach( (list, index) => {
+              newMoviesList[index] = {
+                id: list.id,
+                title: list.title,
+                moviePoster: list.images.large,
+                rating: list.rating,
+              }
             })
             this.setData({
-              highScoreMovies: originalMoviesList.slice(0, 3)
+              [moviesListName]: newMoviesList
             });
           }
         }
       }
     });
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
   }
 })
